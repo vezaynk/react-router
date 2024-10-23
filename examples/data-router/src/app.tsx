a899501c6d6701f6d7e56fa3fdec95303cde48ee
Matt Brophy <matt@brophy.org>
2024-10-23 17:35:02 -0400

    Adopt remaining future flags

--- examples/data-router/src/app.tsx
+++ examples/data-router/src/app.tsx
@@ -58,6 +58,10 @@ const routes: RouteObject[] = [
 let router = createBrowserRouter(routes, {
   future: {
     v7_partialHydration: true,
+    v7_normalizeFormMethod: true,
+    v7_fetcherPersist: true,
+    v7_relativeSplatPath: true,
+    v7_skipActionErrorRevalidation: true,
   },
 });
 
