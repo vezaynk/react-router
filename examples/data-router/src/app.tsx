d862ee0a3bb4cd5995f358bc00cb27cc70911ba0
Matt Brophy <matt@brophy.org>
2024-10-23 17:25:07 -0400

    Move defer route to a manual route module

--- examples/data-router/src/app.tsx
+++ examples/data-router/src/app.tsx
@@ -1,17 +1,5 @@
 import * as React from "react";
-import type { LoaderFunctionArgs } from "react-router-dom";
-import {
-  Await,
-  createBrowserRouter,
-  defer,
-  RouterProvider,
-  useAsyncError,
-  useAsyncValue,
-  useLoaderData,
-  useParams,
-} from "react-router-dom";
-
-import { getTodos } from "./todos";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
 
@@ -51,8 +39,7 @@ let router = createBrowserRouter([
       },
       {
         path: "deferred",
-        loader: deferredLoader,
-        Component: DeferredPage,
+        lazy: () => import("./routes/defer").then(convert),
       },
     ],
   },
@@ -72,101 +59,4 @@ export function sleep(n: number = 500) {
 
 export function Fallback() {
   return <p>Performing initial data load</p>;
-}
-
-// Deferred Data
-interface DeferredRouteLoaderData {
-  critical1: string;
-  critical2: string;
-  lazyResolved: Promise<string>;
-  lazy1: Promise<string>;
-  lazy2: Promise<string>;
-  lazy3: Promise<string>;
-  lazyError: Promise<string>;
-}
-
-const rand = () => Math.round(Math.random() * 100);
-const resolve = (d: string, ms: number) =>
-  new Promise((r) => setTimeout(() => r(`${d} - ${rand()}`), ms));
-const reject = (d: Error | string, ms: number) =>
-  new Promise((_, r) =>
-    setTimeout(() => {
-      if (d instanceof Error) {
-        d.message += ` - ${rand()}`;
-      } else {
-        d += ` - ${rand()}`;
-      }
-      r(d);
-    }, ms)
-  );
-
-export async function deferredLoader() {
-  return defer({
-    critical1: await resolve("Critical 1", 250),
-    critical2: await resolve("Critical 2", 500),
-    lazyResolved: Promise.resolve("Lazy Data immediately resolved - " + rand()),
-    lazy1: resolve("Lazy 1", 1000),
-    lazy2: resolve("Lazy 2", 1500),
-    lazy3: resolve("Lazy 3", 2000),
-    lazyError: reject(new Error("Kaboom!"), 2500),
-  });
-}
-
-export function DeferredPage() {
-  let data = useLoaderData() as DeferredRouteLoaderData;
-  return (
-    <div>
-      {/* Critical data renders immediately */}
-      <p>{data.critical1}</p>
-      <p>{data.critical2}</p>
-
-      {/* Pre-resolved deferred data never triggers the fallback */}
-      <React.Suspense fallback={<p>should not see me!</p>}>
-        <Await resolve={data.lazyResolved}>
-          <RenderAwaitedData />
-        </Await>
-      </React.Suspense>
-
-      {/* Deferred data can be rendered using a component + the useAsyncValue() hook */}
-      <React.Suspense fallback={<p>loading 1...</p>}>
-        <Await resolve={data.lazy1}>
-          <RenderAwaitedData />
-        </Await>
-      </React.Suspense>
-
-      <React.Suspense fallback={<p>loading 2...</p>}>
-        <Await resolve={data.lazy2}>
-          <RenderAwaitedData />
-        </Await>
-      </React.Suspense>
-
-      {/* Or you can bypass the hook and use a render function */}
-      <React.Suspense fallback={<p>loading 3...</p>}>
-        <Await resolve={data.lazy3}>{(data: string) => <p>{data}</p>}</Await>
-      </React.Suspense>
-
-      {/* Deferred rejections render using the useAsyncError hook */}
-      <React.Suspense fallback={<p>loading (error)...</p>}>
-        <Await resolve={data.lazyError} errorElement={<RenderAwaitedError />}>
-          <RenderAwaitedData />
-        </Await>
-      </React.Suspense>
-    </div>
-  );
-}
-
-function RenderAwaitedData() {
-  let data = useAsyncValue() as string;
-  return <p>{data}</p>;
-}
-
-function RenderAwaitedError() {
-  let error = useAsyncError() as Error;
-  return (
-    <p style={{ color: "red" }}>
-      Error (errorElement)!
-      <br />
-      {error.message} {error.stack}
-    </p>
-  );
 }
---
+++ examples/data-router/src/routes/defer.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import {
+  useLoaderData,
+  Await,
+  useAsyncValue,
+  useAsyncError,
+  defer,
+} from "react-router-dom";
+
+interface DeferredRouteLoaderData {
+  critical1: string;
+  critical2: string;
+  lazyResolved: Promise<string>;
+  lazy1: Promise<string>;
+  lazy2: Promise<string>;
+  lazy3: Promise<string>;
+  lazyError: Promise<string>;
+}
+
+const rand = () => Math.round(Math.random() * 100);
+const resolve = (d: string, ms: number) =>
+  new Promise((r) => setTimeout(() => r(`${d} - ${rand()}`), ms));
+const reject = (d: Error | string, ms: number) =>
+  new Promise((_, r) =>
+    setTimeout(() => {
+      if (d instanceof Error) {
+        d.message += ` - ${rand()}`;
+      } else {
+        d += ` - ${rand()}`;
+      }
+      r(d);
+    }, ms)
+  );
+
+export async function clientLoader() {
+  return defer({
+    critical1: await resolve("Critical 1", 250),
+    critical2: await resolve("Critical 2", 500),
+    lazyResolved: Promise.resolve("Lazy Data immediately resolved - " + rand()),
+    lazy1: resolve("Lazy 1", 1000),
+    lazy2: resolve("Lazy 2", 1500),
+    lazy3: resolve("Lazy 3", 2000),
+    lazyError: reject(new Error("Kaboom!"), 2500),
+  });
+}
+
+export default function DeferredPage() {
+  let data = useLoaderData() as DeferredRouteLoaderData;
+  return (
+    <div>
+      {/* Critical data renders immediately */}
+      <p>{data.critical1}</p>
+      <p>{data.critical2}</p>
+
+      {/* Pre-resolved deferred data never triggers the fallback */}
+      <React.Suspense fallback={<p>should not see me!</p>}>
+        <Await resolve={data.lazyResolved}>
+          <RenderAwaitedData />
+        </Await>
+      </React.Suspense>
+
+      {/* Deferred data can be rendered using a component + the useAsyncValue() hook */}
+      <React.Suspense fallback={<p>loading 1...</p>}>
+        <Await resolve={data.lazy1}>
+          <RenderAwaitedData />
+        </Await>
+      </React.Suspense>
+
+      <React.Suspense fallback={<p>loading 2...</p>}>
+        <Await resolve={data.lazy2}>
+          <RenderAwaitedData />
+        </Await>
+      </React.Suspense>
+
+      {/* Or you can bypass the hook and use a render function */}
+      <React.Suspense fallback={<p>loading 3...</p>}>
+        <Await resolve={data.lazy3}>{(data: string) => <p>{data}</p>}</Await>
+      </React.Suspense>
+
+      {/* Deferred rejections render using the useAsyncError hook */}
+      <React.Suspense fallback={<p>loading (error)...</p>}>
+        <Await resolve={data.lazyError} errorElement={<RenderAwaitedError />}>
+          <RenderAwaitedData />
+        </Await>
+      </React.Suspense>
+    </div>
+  );
+}
+
+function RenderAwaitedData() {
+  let data = useAsyncValue() as string;
+  return <p>{data}</p>;
+}
+
+function RenderAwaitedError() {
+  let error = useAsyncError() as Error;
+  return (
+    <p style={{ color: "red" }}>
+      Error (errorElement)!
+      <br />
+      {error.message} {error.stack}
+    </p>
+  );
+}
